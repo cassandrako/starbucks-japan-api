53796d8c9ac307599004b822ab8b3ac1a23bbf4b
CassandraK <ko.cassie@gmail.com>
2023-12-06 17:20:43 -0500

    feat: first push

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData('espresso');
+});
+
+function fetchData(drinkType) {
+    fetch(`https://starbucks-japan.onrender.com/drinks?type=${drinkType}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayData(data);
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+function displayData(data) {
+    const container = document.getElementById('data-container');
+    data.forEach(item => {
+        const element = document.createElement('div');
+        element.innerHTML = `Item: ${item.name}`;
+        container.appendChild(element);
+    });
+}
