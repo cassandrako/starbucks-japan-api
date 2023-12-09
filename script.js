4e256b5befcd683df0081ff4e844344900f13907
CassandraK <ko.cassie@gmail.com>
2023-12-08 22:04:55 -0500

    feat: added menu asset, consoled fetch data, not displaying

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData('espresso');
-});
-
 function fetchData(drinkType) {
     fetch(`https://starbucks-japan.onrender.com/drinks?type=${drinkType}`)
         .then(response => {
@@ -18,18 +14,30 @@ function fetchData(drinkType) {
             return response.json();
         })
         .then(data => {
+            console.log('Fetched data:', data);
             displayData(data);
         })
         .catch(error => {
             console.error('Fetch error:', error);
+            displayError(error);
         });
 }
 
 function displayData(data) {
     const container = document.getElementById('data-container');
+    container.innerHTML = ''; 
     data.forEach(item => {
         const element = document.createElement('div');
-        element.innerHTML = `Item: ${item.name}`; 
+        element.innerHTML = `Item: ${item.name}`;
         container.appendChild(element);
     });
+}
+
+function displayError(error) {
+    const container = document.getElementById('data-container');
+    container.innerHTML = ''; 
+    const errorElement = document.createElement('div');
+    errorElement.style.color = 'red';
+    errorElement.innerHTML = `Error: ${error.message}`;
+    container.appendChild(errorElement);
 }
