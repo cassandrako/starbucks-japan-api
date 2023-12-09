da81831299d6bf838c88cd2ffd7835d528ab5ec9
CassandraK <ko.cassie@gmail.com>
2023-12-08 21:54:16 -0500

    feat: added assets for bg, and styled the buttons.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('fetch-button').addEventListener('click', () => {
+        const selectedDrink = document.getElementById('drink-type').value;
+        fetchData(selectedDrink);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
     fetchData('espresso');
 });
 
@@ -22,7 +29,7 @@ function displayData(data) {
     const container = document.getElementById('data-container');
     data.forEach(item => {
         const element = document.createElement('div');
-        element.innerHTML = `Item: ${item.name}`;
+        element.innerHTML = `Item: ${item.name}`; 
         container.appendChild(element);
     });
 }
